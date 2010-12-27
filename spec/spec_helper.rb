e0f09e232418a9d471e9af0a4babb6f10ccad96b
Arto Bendiken <arto@bendiken.net>
2010-12-28 00:07:57 +0100

    Updated the spec helper for RSpec 2.x compatibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'linkeddata'
 require 'rdf/spec'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.exclusion_filter = {:ruby => lambda { |version|
+    RUBY_VERSION.to_s !~ /^#{version}/
+  }}
 end
+
+include LinkedData
