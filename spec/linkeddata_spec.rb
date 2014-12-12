861480524a1725d9a858e285d499f6b8768d8f9c
Gregg Kellogg <gregg@greggkellogg.net>
2014-12-12 15:59:03 -0800

    Update dependencies.

--- spec/linkeddata_spec.rb
+++ spec/linkeddata_spec.rb
@@ -2,13 +2,13 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe LinkedData do
   it "should have a version" do
-    lambda { LinkedData::VERSION }.should_not raise_error
-    lambda { LinkedData::VERSION.to_s }.should_not raise_error
-    lambda { LinkedData::VERSION.to_a }.should_not raise_error
+    expect { LinkedData::VERSION }.not_to raise_error
+    expect { LinkedData::VERSION.to_s }.not_to raise_error
+    expect { LinkedData::VERSION.to_a }.not_to raise_error
   end
 
   it "should have the same version as RDF.rb" do
-    LinkedData::VERSION.to_s.should == RDF::VERSION.to_s
-    LinkedData::VERSION.to_a.should == RDF::VERSION.to_a
+    expect(LinkedData::VERSION.to_s).to eq RDF::VERSION.to_s
+    expect(LinkedData::VERSION.to_a).to eq RDF::VERSION.to_a
   end
 end
